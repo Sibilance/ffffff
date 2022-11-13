2b00564d94330f6b2739d4341dc87a328acf113e
Talia Stocks <taliastocks@gmail.com>
2022-11-13 14:43:02 -0500

    recursively decode yaml.Node

--- pkg/yamlreader/yamlreader.go
+++ pkg/yamlreader/yamlreader.go
@@ -7,23 +7,25 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func ReadFile(fileName string) ([]yaml.Node, error) {
+func ReadFile(fileName string) (node Node, err error) {
+	node.FileName = fileName
 	file, err := os.Open(fileName)
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	var documents []yaml.Node
+	// Read all the documents from the yaml file.
+	var documents []Node
 	decoder := yaml.NewDecoder(file)
 	for {
-		var document yaml.Node
-		if err := decoder.Decode(&document); err != nil {
+		var document Node
+		if err = decoder.Decode(&document); err != nil {
 			if err != io.EOF {
-				return nil, err
+				return
 			}
 			break
 		}
 		documents = append(documents, document)
 	}
-	return documents, nil
+	return
 }
