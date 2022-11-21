12496339e295f4b84e1d72ce271c84ab3db34693
Talia Stocks <928827+taliastocks@users.noreply.github.com>
2022-11-21 14:30:14 -0500

    next (#1)
    * recursively decode yaml.Node
    
    * test
    
    * test decoding null documents
    
    * make ReadFile a method of node
    
    * decode more data types
    
    * test reading multiple documents
    
    * test missing files and aliases
    
    * test sequences and maps
    
    * test scalar types
    
    * test and fix edge case of integer overflow

--- pkg/yamlreader/yamlreader.go
+++ pkg/yamlreader/yamlreader.go
@@ -1,29 +1,222 @@
 package yamlreader
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
 
-func ReadFile(fileName string) ([]yaml.Node, error) {
+type Kind int8
+
+const (
+	UndefinedNode Kind = iota
+	SequenceNode
+	MappingNode
+	BooleanNode
+	IntegerNode
+	FloatNode
+	StringNode
+	NullNode
+
+	SeqTag   string = "!!seq"
+	MapTag   string = "!!map"
+	BoolTag  string = "!!bool"
+	IntTag   string = "!!int"
+	FloatTag string = "!!float"
+	StrTag   string = "!!str"
+	NullTag  string = "!!null"
+)
+
+func (kind Kind) String() string {
+	switch kind {
+	case UndefinedNode:
+		return "UndefinedNode"
+	case SequenceNode:
+		return "SequenceNode"
+	case MappingNode:
+		return "MappingNode"
+	case BooleanNode:
+		return "BooleanNode"
+	case IntegerNode:
+		return "IntegerNode"
+	case FloatNode:
+		return "FloatNode"
+	case StringNode:
+		return "StringNode"
+	case NullNode:
+		return "NullNode"
+	}
+	return "unknown"
+}
+
+type Node struct {
+	Name     string
+	FileName string
+	Line     int
+	Column   int
+	Comment  string
+	Kind     Kind
+	Tag      string
+	Sequence []Node
+	Mapping  map[string]Node
+	Bool     bool
+	Int      int64
+	Float    float64
+	Str      string
+}
+
+func (n *Node) String() string {
+	return fmt.Sprintf("%s (%s:%d:%d)", n.Name, n.FileName, n.Line, n.Column)
+}
+
+func (n *Node) ReadFile(fileName string) error {
+	n.FileName = fileName
+
 	file, err := os.Open(fileName)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	var documents []yaml.Node
+	return n.ReadStream(file)
+}
+
+func (n *Node) ReadStream(file io.Reader) error {
+	// Read all the documents from the yaml file.
+	var documents []*yaml.Node
 	decoder := yaml.NewDecoder(file)
 	for {
 		var document yaml.Node
 		if err := decoder.Decode(&document); err != nil {
 			if err != io.EOF {
-				return nil, err
+				return fmt.Errorf("%s: %s", n, err)
 			}
 			break
 		}
-		documents = append(documents, document)
+		documents = append(documents, &document)
 	}
-	return documents, nil
+
+	if len(documents) == 1 {
+		if err := n.UnmarshalYAML(documents[0]); err != nil {
+			return err
+		}
+	} else if len(documents) > 1 {
+		n.Kind = SequenceNode
+		n.Tag = SeqTag
+		for i, document := range documents {
+			innerNode := Node{
+				Name:     fmt.Sprintf("%s[%d]", n.Name, i),
+				FileName: n.FileName,
+			}
+			if err := innerNode.UnmarshalYAML(document); err != nil {
+				return err
+			}
+			n.Sequence = append(n.Sequence, innerNode)
+		}
+	} else {
+		n.Kind = NullNode
+		n.Tag = NullTag
+	}
+
+	return nil
+}
+
+func (n *Node) UnmarshalYAML(yamlNode *yaml.Node) error {
+	recursionDetection := make(map[*yaml.Node]struct{})
+	return n.unmarshalYAML(yamlNode, recursionDetection)
+}
+
+func (n *Node) unmarshalYAML(yamlNode *yaml.Node, recursionDetection map[*yaml.Node]struct{}) error {
+	n.Tag = yamlNode.ShortTag()
+	n.Line = yamlNode.Line
+	n.Column = yamlNode.Column
+	n.Comment = strings.Trim(yamlNode.HeadComment+"\n\n"+yamlNode.LineComment, "\n")
+
+	switch yamlNode.Kind {
+	case yaml.DocumentNode:
+		if len(yamlNode.Content) != 1 {
+			return fmt.Errorf("%s: expected one node in document, got %d", n, len(yamlNode.Content))
+		}
+		return n.unmarshalYAML(yamlNode.Content[0], recursionDetection)
+
+	case yaml.AliasNode:
+		// If we've encountered this particular AliasNode before, it must be a recursive alias.
+		// Note that we track the AliasNode itself, not the node it is aliased to, as multiple
+		// pointers to the same node are valid and don't indicate recursion.
+		if _, contained := recursionDetection[yamlNode]; contained {
+			return fmt.Errorf("%s: recursive alias detected", n)
+		}
+		recursionDetection[yamlNode] = struct{}{}
+		// Follow the alias, but keep these attributes from the alias node.
+		line, column, comment := n.Line, n.Column, n.Comment
+		err := n.unmarshalYAML(yamlNode.Alias, recursionDetection)
+		n.Line, n.Column, n.Comment = line, column, comment
+		return err
+
+	case yaml.SequenceNode:
+		n.Kind = SequenceNode
+		for i, innerYamlNode := range yamlNode.Content {
+			innerNode := Node{
+				Name:     fmt.Sprintf("%s[%d]", n.Name, i),
+				FileName: n.FileName,
+			}
+			if err := innerNode.unmarshalYAML(innerYamlNode, recursionDetection); err != nil {
+				return err
+			}
+			n.Sequence = append(n.Sequence, innerNode)
+		}
+
+	case yaml.MappingNode:
+		n.Kind = MappingNode
+		n.Mapping = make(map[string]Node)
+		mapping := map[string]yaml.Node{}
+		if err := yamlNode.Decode(mapping); err != nil {
+			return fmt.Errorf("%s: %s", n, err)
+		}
+		for k, innerYamlNode := range mapping {
+			innerNode := Node{
+				Name:     fmt.Sprintf("%s.%s", n.Name, k),
+				FileName: n.FileName,
+			}
+			if err := innerNode.unmarshalYAML(&innerYamlNode, recursionDetection); err != nil {
+				return err
+			}
+			n.Mapping[k] = innerNode
+		}
+
+	case yaml.ScalarNode:
+		n.Str = yamlNode.Value
+		switch n.Tag {
+		case BoolTag:
+			n.Kind = BooleanNode
+			yamlNode.Decode(&n.Bool)
+		case IntTag:
+			n.Kind = IntegerNode
+			// We have to check for overflows of positive integers because the yaml
+			// library detects anything that fits in int64 or uint64 as !!int.
+			var overflowCheck uint64
+			yamlNode.Decode(&overflowCheck)
+			if overflowCheck > ^uint64(0)>>1 {
+				n.Kind = FloatNode
+				n.Tag = FloatTag
+				yamlNode.Decode(&n.Float)
+			} else {
+				yamlNode.Decode(&n.Int)
+			}
+		case FloatTag:
+			n.Kind = FloatNode
+			yamlNode.Decode(&n.Float)
+		case NullTag:
+			n.Kind = NullNode
+		default:
+			n.Kind = StringNode
+		}
+
+	default:
+		return fmt.Errorf("%s: unknown node kind", n)
+	}
+
+	return nil
 }
--- pkg/yamlreader/yamlreader_test.go
+++ pkg/yamlreader/yamlreader_test.go
@@ -3,45 +3,559 @@ package yamlreader
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
-
-	"gopkg.in/yaml.v3"
 )
 
-func TestReadFile(t *testing.T) {
+func getTestFile(t *testing.T) string {
 	cwd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	testFile := filepath.Join(cwd, "test", t.Name()+".yaml")
+	return filepath.Join(cwd, "test", t.Name()+".yaml")
+}
 
-	nodes, err := ReadFile(testFile)
+func compareNodes(t *testing.T, actual, expected *Node) {
+	if actual.Name != expected.Name {
+		t.Fatalf("%s: expected Name=%s, got %s", actual.Name, expected.Name, actual.Name)
+	}
+
+	if actual.FileName != expected.FileName {
+		t.Fatalf("%s: expected FileName=%s, got %s", actual.Name, expected.FileName, actual.FileName)
+	}
+
+	if actual.Line != expected.Line {
+		t.Fatalf("%s: expected Line=%d, got %d", actual.Name, expected.Line, actual.Line)
+	}
+
+	if actual.Column != expected.Column {
+		t.Fatalf("%s: expected Column=%d, got %d", actual.Name, expected.Column, actual.Column)
+	}
+
+	if actual.Comment != expected.Comment {
+		t.Fatalf("%s: expected Comment:\n%s\n\ngot:\n%s", actual.Name, expected.Comment, actual.Comment)
+	}
+
+	if actual.Kind != expected.Kind {
+		t.Fatalf("%s: expected Kind=%s, got %s", actual.Name, expected.Kind, actual.Kind)
+	}
+
+	if actual.Tag != expected.Tag {
+		t.Fatalf("%s: expected Tag=%s, got %s", actual.Name, expected.Tag, actual.Tag)
+	}
+
+	if len(actual.Sequence) != len(expected.Sequence) {
+		t.Fatalf("%s: expected len(Sequence)=%d, got %d", actual.Name, len(expected.Sequence), len(actual.Sequence))
+	}
+
+	for i, innerExpected := range expected.Sequence {
+		innerActual := actual.Sequence[i]
+		compareNodes(t, &innerActual, &innerExpected)
+	}
+
+	if len(actual.Mapping) != len(expected.Mapping) {
+		t.Fatalf("%s: expected len(Mapping)=%d, got %d", actual.Name, len(expected.Mapping), len(actual.Mapping))
+	}
+
+	for k, innerExpected := range expected.Mapping {
+		innerActual := actual.Mapping[k]
+		compareNodes(t, &innerActual, &innerExpected)
+	}
+
+	if actual.Bool != expected.Bool {
+		t.Fatalf("%s: expected Bool=%t, got %t", actual.Name, expected.Bool, actual.Bool)
+	}
+
+	if actual.Int != expected.Int {
+		t.Fatalf("%s: expected Int=%d, got %d", actual.Name, expected.Int, actual.Int)
+	}
+
+	if actual.Float != expected.Float {
+		t.Fatalf("%s: expected Float=%f, got %f", actual.Name, expected.Float, actual.Float)
+	}
+
+	if actual.Str != expected.Str {
+		t.Fatalf("%s: expected Str=%s, got %s", actual.Name, expected.Str, actual.Str)
+	}
+}
+
+func ReadTestFile(t *testing.T) *Node {
+	testFile := getTestFile(t)
+
+	actual := &Node{
+		Name: "test",
+	}
+	err := actual.ReadFile(testFile)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(nodes) != 1 {
-		t.Fatal("expected exactly one document, got:", len(nodes))
+	return actual
+}
+
+// Test that reading a missing file returns an error.
+func TestReadFileMissing(t *testing.T) {
+	testFile := getTestFile(t)
+
+	actual := &Node{}
+	err := actual.ReadFile(testFile)
+	if err == nil {
+		t.Fatalf("no error returned for missing file")
+	}
+	if !strings.Contains(err.Error(), "no such file or directory") {
+		t.Fatalf("wrong error returned: %s", err.Error())
+	}
+}
+
+// Test that reading a file containing a single document returns the only
+// node found in that document. Checks that comments, line numbers, Kinds,
+// and Tags are preserved properly.
+func TestReadFile(t *testing.T) {
+	actual := ReadTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: getTestFile(t),
+		Line:     2,
+		Column:   1,
+		Comment:  "# Head Comment\n\n# Line Comment",
+		Kind:     StringNode,
+		Tag:      "!!str",
+		Str:      "Hello World",
 	}
 
-	node := nodes[0]
+	compareNodes(t, actual, expected)
+}
 
-	if node.Kind != yaml.DocumentNode {
-		t.Fatal("expected DocumentNode, got:", node.Kind)
+// Test that reading a file containing 'null' returns a null node.
+func TestReadFileNull(t *testing.T) {
+	actual := ReadTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: getTestFile(t),
+		Line:     1,
+		Column:   1,
+		Kind:     NullNode,
+		Tag:      "!!null",
+		Str:      "null",
 	}
 
-	if len(node.Content) != 1 {
-		t.Fatal("expected one child, got:", len(node.Content))
+	compareNodes(t, actual, expected)
+}
+
+// Test that reading an empty file returns a null node.
+func TestReadFileEmpty(t *testing.T) {
+	actual := ReadTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: getTestFile(t),
+		Kind:     NullNode,
+		Tag:      "!!null",
 	}
 
-	child := node.Content[0]
+	compareNodes(t, actual, expected)
+}
 
-	if child.Kind != yaml.ScalarNode {
-		t.Fatal("expected ScalarNode, got:", child.Kind)
+// Test that reading a file with multiple documents returns a sequence node.
+func TestReadFileMultipleDocuments(t *testing.T) {
+	actual := ReadTestFile(t)
+	testFile := getTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: testFile,
+		Kind:     SequenceNode,
+		Tag:      "!!seq",
+		Sequence: []Node{
+			{
+				Name:     "test[0]",
+				FileName: testFile,
+				Line:     2,
+				Column:   1,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "Document one",
+			},
+			{
+				Name:     "test[1]",
+				FileName: testFile,
+				Line:     4,
+				Column:   1,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "Document two",
+			},
+		},
 	}
 
-	if child.Value != "Hello World" {
-		t.Fatal("expected Hello World, got:", child.Value)
+	compareNodes(t, actual, expected)
+}
+
+// Test that aliases are followed correctly.
+func TestReadFileAlias(t *testing.T) {
+	actual := ReadTestFile(t)
+	testFile := getTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: testFile,
+		Line:     1,
+		Column:   1,
+		Kind:     SequenceNode,
+		Tag:      "!!seq",
+		Sequence: []Node{
+			{
+				Name:     "test[0]",
+				FileName: testFile,
+				Line:     1,
+				Column:   3,
+				Comment:  "# Comment 1",
+				Kind:     StringNode,
+				Tag:      "!MyTag",
+				Str:      "Thing to be copied.",
+			},
+			{
+				Name:     "test[1]",
+				FileName: testFile,
+				Line:     2,
+				Column:   3,
+				Comment:  "# Comment 2",
+				Kind:     StringNode,
+				Tag:      "!MyTag",
+				Str:      "Thing to be copied.",
+			},
+		},
 	}
+
+	compareNodes(t, actual, expected)
+}
+
+// Test that recursive aliases are detected and return an error.
+func TestReadFileRecursiveAlias(t *testing.T) {
+	testFile := getTestFile(t)
+
+	actual := &Node{}
+	err := actual.ReadFile(testFile)
+	if err == nil {
+		t.Fatalf("no error returned for recursive alias")
+	}
+	if !strings.Contains(err.Error(), "recursive alias detected") {
+		t.Fatalf("wrong error returned: %s", err.Error())
+	}
+}
+
+// Test that sequences are decoded correctly.
+func TestSequence(t *testing.T) {
+	actual := ReadTestFile(t)
+	testFile := getTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: testFile,
+		Line:     1,
+		Column:   1,
+		Kind:     SequenceNode,
+		Tag:      "!!seq",
+		Sequence: []Node{
+			{
+				Name:     "test[0]",
+				FileName: testFile,
+				Line:     1,
+				Column:   3,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "Item 1",
+			},
+			{
+				Name:     "test[1]",
+				FileName: testFile,
+				Line:     2,
+				Column:   3,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "Item 2",
+			},
+		},
+	}
+
+	compareNodes(t, actual, expected)
+}
+
+// Test that mappings are decoded correctly.
+func TestMapping(t *testing.T) {
+	actual := ReadTestFile(t)
+	testFile := getTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: testFile,
+		Line:     1,
+		Column:   1,
+		Kind:     MappingNode,
+		Tag:      "!!map",
+		Mapping: map[string]Node{
+			"key 1": {
+				Name:     "test.key 1",
+				FileName: testFile,
+				Line:     1,
+				Column:   8,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "value 1",
+			},
+			"key 2": {
+				Name:     "test.key 2",
+				FileName: testFile,
+				Line:     2,
+				Column:   8,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "value 2",
+			},
+		},
+	}
+
+	compareNodes(t, actual, expected)
+}
+
+// Test that different scalar types are decoded correctly.
+func TestScalar(t *testing.T) {
+	actual := ReadTestFile(t)
+	testFile := getTestFile(t)
+
+	expected := &Node{
+		Name:     "test",
+		FileName: testFile,
+		Line:     1,
+		Column:   1,
+		Kind:     SequenceNode,
+		Tag:      "!!seq",
+		Sequence: []Node{
+			{
+				Name:     "test[0]",
+				FileName: testFile,
+				Line:     1,
+				Column:   3,
+				Kind:     BooleanNode,
+				Tag:      "!!bool",
+				Str:      "true",
+				Bool:     true,
+			},
+			{
+				Name:     "test[1]",
+				FileName: testFile,
+				Line:     2,
+				Column:   3,
+				Kind:     BooleanNode,
+				Tag:      "!!bool",
+				Str:      "false",
+				Bool:     false,
+			},
+			{
+				Name:     "test[2]",
+				FileName: testFile,
+				Line:     3,
+				Column:   3,
+				Kind:     BooleanNode,
+				Tag:      "!!bool",
+				Str:      "False",
+				Bool:     false,
+			},
+			{
+				Name:     "test[3]",
+				FileName: testFile,
+				Line:     4,
+				Column:   3,
+				Kind:     BooleanNode,
+				Tag:      "!!bool",
+				Str:      "True",
+				Bool:     true,
+			},
+			{
+				Name:     "test[4]",
+				FileName: testFile,
+				Line:     5,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "0",
+				Int:      0,
+			},
+			{
+				Name:     "test[5]",
+				FileName: testFile,
+				Line:     6,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "1",
+				Int:      1,
+			},
+			{
+				Name:     "test[6]",
+				FileName: testFile,
+				Line:     7,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "+1",
+				Int:      1,
+			},
+			{
+				Name:     "test[7]",
+				FileName: testFile,
+				Comment:  "# largest int64",
+				Line:     8,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "9223372036854775807",
+				Int:      9223372036854775807,
+			},
+			{
+				Name:     "test[8]",
+				FileName: testFile,
+				Comment:  "# smallest int64",
+				Line:     9,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "-9223372036854775808",
+				Int:      -9223372036854775808,
+			},
+			{
+				Name:     "test[9]",
+				FileName: testFile,
+				Comment:  "# binary",
+				Line:     10,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "0b1101",
+				Int:      0b1101,
+			},
+			{
+				Name:     "test[10]",
+				FileName: testFile,
+				Comment:  "# octal",
+				Line:     11,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "0o14",
+				Int:      0o14,
+			},
+			{
+				Name:     "test[11]",
+				FileName: testFile,
+				Comment:  "# hex",
+				Line:     12,
+				Column:   3,
+				Kind:     IntegerNode,
+				Tag:      "!!int",
+				Str:      "0xFF",
+				Int:      0xFF,
+			},
+			{
+				Name:     "test[12]",
+				FileName: testFile,
+				Comment:  "# Pi",
+				Line:     13,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "3.14159",
+				Float:    3.14159,
+			},
+			{
+				Name:     "test[13]",
+				FileName: testFile,
+				Comment:  "# Avogadro",
+				Line:     14,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "6.022e+23",
+				Float:    6.022e+23,
+			},
+			{
+				Name:     "test[14]",
+				FileName: testFile,
+				Comment:  "# null",
+				Line:     15,
+				Column:   3,
+				Kind:     NullNode,
+				Tag:      "!!null",
+				Str:      "~",
+			},
+			{
+				Name:     "test[15]",
+				FileName: testFile,
+				Line:     16,
+				Column:   3,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "A string",
+			},
+			{
+				Name:     "test[16]",
+				FileName: testFile,
+				Comment:  "# Also a string",
+				Line:     17,
+				Column:   3,
+				Kind:     StringNode,
+				Tag:      "!!str",
+				Str:      "123",
+			},
+			{
+				Name:     "test[17]",
+				FileName: testFile,
+				Comment:  "# overflow -> float",
+				Line:     18,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "-9223372036854775809",
+				Float:    -9223372036854775809,
+			},
+			{
+				Name:     "test[18]",
+				FileName: testFile,
+				Comment:  "# overflow -> float",
+				Line:     19,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "9223372036854775808",
+				Float:    9223372036854775808,
+			},
+			{
+				Name:     "test[19]",
+				FileName: testFile,
+				Comment:  "# largest uint64 -> float",
+				Line:     20,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "18446744073709551615",
+				Float:    18446744073709551615,
+			},
+			{
+				Name:     "test[20]",
+				FileName: testFile,
+				Comment:  "# one greater -> float",
+				Line:     21,
+				Column:   3,
+				Kind:     FloatNode,
+				Tag:      "!!float",
+				Str:      "18446744073709551616",
+				Float:    18446744073709551616,
+			},
+		},
+	}
+
+	compareNodes(t, actual, expected)
 }
