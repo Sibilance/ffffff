f4a919c5c7e646ec50ce279edebf98ebcbf358a0
Talia Stocks <928827+taliastocks@users.noreply.github.com>
2022-11-21 16:42:29 -0500

    ReadDirectory (#2)

--- pkg/yamlreader/yamlreader.go
+++ pkg/yamlreader/yamlreader.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -72,8 +73,67 @@ func (n *Node) String() string {
 	return fmt.Sprintf("%s (%s:%d:%d)", n.Name, n.FileName, n.Line, n.Column)
 }
 
+func (n *Node) ReadDirectory(dirName string) error {
+	n.FileName = dirName
+	n.Kind = MappingNode
+	n.Mapping = make(map[string]Node)
+
+	if n.Name == "" {
+		n.Name = nameFromPath(dirName, false)
+	}
+
+	dirEntries, err := os.ReadDir(dirName)
+	if err != nil {
+		return err
+	}
+
+	for _, dirEntry := range dirEntries {
+		childName := dirEntry.Name()
+
+		// Ignore hidden files.
+		if strings.HasPrefix(childName, ".") {
+			continue
+		}
+
+		childPath := path.Join(dirName, childName)
+		child := Node{}
+
+		// If the child is a directory, load it recursively.
+		if dirEntry.IsDir() {
+			child.Name = n.Name + "." + childName
+			err := child.ReadDirectory(childPath)
+			if err != nil {
+				return err
+			}
+
+			// Prune empty directories.
+			if len(child.Mapping) == 0 {
+				continue
+			}
+		} else if strings.HasSuffix(childPath, ".yaml") {
+			childName = childName[:len(childName)-5]
+			child.Name = n.Name + "." + childName
+
+			err := child.ReadFile(childPath)
+			if err != nil {
+				return err
+			}
+		} else {
+			continue // Nothing to do here.
+		}
+
+		n.Mapping[childName] = child
+	}
+
+	return nil
+}
+
 func (n *Node) ReadFile(fileName string) error {
 	n.FileName = fileName
+
+	if n.Name == "" {
+		n.Name = nameFromPath(fileName, true)
+	}
 
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -219,4 +279,15 @@ func (n *Node) unmarshalYAML(yamlNode *yaml.Node, recursionDetection map[*yaml.N
 	}
 
 	return nil
+}
+
+func nameFromPath(fileName string, isFile bool) string {
+	name := path.Base(fileName)
+
+	// Remove .yaml file extension if it exists.
+	if isFile && strings.HasSuffix(strings.ToLower(name), ".yaml") {
+		name = name[:len(name)-5]
+	}
+
+	return name
 }
