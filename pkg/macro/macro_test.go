a5d74e3c69bb0749a168296c659f2ef436dcc2f1
Talia Stocks <928827+taliastocks@users.noreply.github.com>
2023-07-04 15:55:50 -0400

    getyamltestcases (#35)

--- pkg/macro/macro_test.go
+++ pkg/macro/macro_test.go
@@ -3,35 +3,30 @@ package macro
 import (
 	"testing"
 
-	"github.com/sibilance/ffffff/pkg/readyaml"
 	"github.com/sibilance/ffffff/pkg/testhelpers"
-	"gopkg.in/yaml.v3"
 )
 
+func testProcessDocuments(t *testing.T, count int) {
+	inputs, outputs := testhelpers.GetYamlTestCases(t, count)
+
+	for i, input := range inputs {
+		output := outputs[i]
+
+		err := ProcessDocuments(&Context{}, &input)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = testhelpers.CompareNodeLists(
+			input,
+			output,
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 func TestVoidDocument(t *testing.T) {
-	documents, err := readyaml.ReadFile(testhelpers.GetTestFile(t))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = ProcessDocuments(&Context{}, &documents)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testhelpers.CompareNodeLists(
-		documents,
-		[]*yaml.Node{
-			{
-				Kind: yaml.DocumentNode,
-				Content: []*yaml.Node{
-					{
-						Kind:  yaml.ScalarNode,
-						Tag:   "!!str",
-						Value: "first non-void document",
-					},
-				},
-			},
-		},
-	)
+	testProcessDocuments(t, 1)
 }
