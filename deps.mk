457376cd009d42cf6d1cf235bd591a3a720891ed
Talia Stocks <taliastocks@gmail.com>
2024-01-06 17:46:47 -0500

    put object files into build directory

--- deps.mk
+++ deps.mk
@@ -1,8 +1,8 @@
-environment.o: environment.h lua/install
-error.o: error.h libyaml/install
-event.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h
-executor.o: error.h event.h executor.h libyaml/install lua/install parser.h
-main.o: environment.h error.h event.h executor.h libyaml/install lua/install parser.h render.h test.h
-parser.o: error.h libyaml/install parser.h
-render.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h
-test.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h test.h
+build/environment.o: environment.h lua/install
+build/error.o: error.h libyaml/install
+build/event.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h
+build/executor.o: error.h event.h executor.h libyaml/install lua/install parser.h
+build/main.o: environment.h error.h event.h executor.h libyaml/install lua/install parser.h render.h test.h
+build/parser.o: error.h libyaml/install parser.h
+build/render.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h
+build/test.o: error.h event.h executor.h libyaml/install lua/install parser.h render.h test.h
