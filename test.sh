fc0c750b4758c78a02dd11c184be0d40ef305bb7
Talia Stocks <928827+taliastocks@users.noreply.github.com>
2024-01-09 20:29:31 -0500

    make separate identity test (#69)

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ build/main.out -i testcases/call.yaml -t
 # build/main.out -i testcases/eval.yaml -t
 # build/main.out -i testcases/for.yaml -t
 build/main.out -i testcases/formatting.yaml -t
+build/main.out -i testcases/identity.yaml -t
 # build/main.out -i testcases/if.yaml -t
