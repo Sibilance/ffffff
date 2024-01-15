0cf0a1d7308dfb7dac78d9c5f59db8ecf0634bfc
Talia Stocks <taliastocks@gmail.com>
2024-01-15 13:34:57 -0500

    explicitly expect particular documents in order

--- test.c
+++ test.c
@@ -12,113 +12,105 @@ int yl_test_stream(yl_execution_context_t *ctx)
 {
     yaml_event_t next_event = {0};
 
-    yl_event_consumer_t saved_consumer = {0};
-    bool recording_actual = true;
     yl_event_record_t actual_events = {0};
     yl_event_record_t expected_events = {0};
     char *expected_rendering = NULL, *actual_rendering = NULL;
 
-    // TODO: Currently just copied from yl_execute_stream().
-    // The challenge here is how to implement the !testcases use-case.
-    // There is no functionality to "rewind" the event stream, so we
-    // need to create one in order to allow the same document to be
-    // executed multiple times with different global variables.
-    // The output also has to be captured and compared, which is already
-    // possible using the callback handler; however, we'll also want
-    // to wrap the existing handler so the user can see the test output.
-    bool done = false;
-    while (!done) {
+    if (!ctx->producer(ctx->producer_data, &next_event, &ctx->err))
+        goto error;
+
+    if (next_event.type != YAML_STREAM_START_EVENT) {
+        ctx->err.type = YL_PARSER_ERROR;
+        ctx->err.line = next_event.start_mark.line;
+        ctx->err.column = next_event.start_mark.column;
+        ctx->err.context = "While trying to read a stream, got unexpected event";
+        ctx->err.message = yl_event_name(next_event.type);
+        goto error;
+    }
+
+    if (!ctx->consumer.callback(ctx->consumer.data, &next_event, NULL, &ctx->err))
+        goto error;
+
+    while (true) {
+        size_t line = 0, column = 0;
         if (!ctx->producer(ctx->producer_data, &next_event, &ctx->err))
             goto error;
 
         switch (next_event.type) {
-        case YAML_STREAM_START_EVENT:
-            if (!ctx->consumer.callback(ctx->consumer.data, &next_event, NULL, &ctx->err))
+        case YAML_DOCUMENT_START_EVENT:
+            line = next_event.start_mark.line;
+            column = next_event.start_mark.column;
+
+            if (!yl_test_record_document(ctx, &next_event, &actual_events))
                 goto error;
+
             break;
-        case YAML_DOCUMENT_START_EVENT: {
-            saved_consumer = ctx->consumer;
 
-            ctx->consumer.callback = (yl_event_consumer_callback_t *)yl_record_event;
-            ctx->consumer.data = recording_actual ? &actual_events : &expected_events;
-
-            size_t line = next_event.start_mark.line;
-            size_t column = next_event.start_mark.column;
-
-            if (!yl_execute_document(ctx, &next_event))
-                goto error;
-
-            ctx->consumer = saved_consumer;
-
-            if (recording_actual) {
-                recording_actual = false;
-            } else {
-                actual_rendering = yl_event_record_to_string(&actual_events, &ctx->err);
-                if (actual_rendering == NULL)
-                    goto error;
-
-                expected_rendering = yl_event_record_to_string(&expected_events, &ctx->err);
-                if (expected_rendering == NULL)
-                    goto error;
-
-                // Consume both the sets of events.
-                for (size_t i = 0; i < actual_events.length; ++i) {
-                    if (!ctx->consumer.callback(ctx->consumer.data, &actual_events.events[i], NULL, &ctx->err))
-                        goto error;
-                }
-                for (size_t i = 0; i < expected_events.length; ++i) {
-                    if (!ctx->consumer.callback(ctx->consumer.data, &expected_events.events[i], NULL, &ctx->err))
-                        goto error;
-                }
-
-                if (strcmp(actual_rendering, expected_rendering) != 0) {
-                    ctx->err.type = YL_ASSERTION_ERROR;
-                    ctx->err.line = line;
-                    ctx->err.column = column;
-                    ctx->err.context = "While comparing rendered documents";
-                    ctx->err.message = "actual document differs from expected document";
-                    goto error;
-                }
-
-                free(actual_rendering);
-                actual_rendering = NULL;
-                free(expected_rendering);
-                expected_rendering = NULL;
-                yl_event_record_delete(&actual_events);
-                yl_event_record_delete(&expected_events);
-                recording_actual = true;
-            }
-        } break;
         case YAML_STREAM_END_EVENT:
-            if (!recording_actual) {
-                ctx->err.type = YL_EXECUTION_ERROR;
-                ctx->err.line = next_event.start_mark.line;
-                ctx->err.column = next_event.start_mark.column;
-                ctx->err.context = "While testing a stream, got test case but no expected output";
-                ctx->err.message = yl_event_name(next_event.type);
-                goto error;
-            }
-
             if (!ctx->consumer.callback(ctx->consumer.data, &next_event, NULL, &ctx->err))
                 goto error;
+            goto done;
 
-            done = true;
-            break;
         default:
-            ctx->err.type = YL_EXECUTION_ERROR;
+            ctx->err.type = YL_PARSER_ERROR;
             ctx->err.line = next_event.start_mark.line;
             ctx->err.column = next_event.start_mark.column;
-            ctx->err.context = "While testing a stream, got unexpected event";
+            ctx->err.context = "While trying to read a document, got unexpected event";
             ctx->err.message = yl_event_name(next_event.type);
             goto error;
         }
 
-        yaml_event_delete(&next_event);
+        actual_rendering = yl_event_record_to_string(&actual_events, &ctx->err);
+        if (actual_rendering == NULL)
+            goto error;
+        for (size_t i = 0; i < actual_events.length; ++i) {
+            if (!ctx->consumer.callback(ctx->consumer.data, &actual_events.events[i], NULL, &ctx->err))
+                goto error;
+        }
+
+        if (!ctx->producer(ctx->producer_data, &next_event, &ctx->err))
+            goto error;
+
+        switch (next_event.type) {
+        case YAML_DOCUMENT_START_EVENT:
+            if (!yl_test_record_document(ctx, &next_event, &expected_events))
+                goto error;
+            break;
+
+        default:
+            ctx->err.type = YL_PARSER_ERROR;
+            ctx->err.line = next_event.start_mark.line;
+            ctx->err.column = next_event.start_mark.column;
+            ctx->err.context = "While trying to read a document, got unexpected event";
+            ctx->err.message = yl_event_name(next_event.type);
+            goto error;
+        }
+
+        expected_rendering = yl_event_record_to_string(&expected_events, &ctx->err);
+        if (expected_rendering == NULL)
+            goto error;
+        for (size_t i = 0; i < expected_events.length; ++i) {
+            if (!ctx->consumer.callback(ctx->consumer.data, &expected_events.events[i], NULL, &ctx->err))
+                goto error;
+        }
+
+        if (strcmp(actual_rendering, expected_rendering) != 0) {
+            ctx->err.type = YL_ASSERTION_ERROR;
+            ctx->err.line = line;
+            ctx->err.column = column;
+            ctx->err.context = "While comparing rendered documents";
+            ctx->err.message = "actual document differs from expected document";
+            goto error;
+        }
+
+        free(actual_rendering);
+        actual_rendering = NULL;
+        free(expected_rendering);
+        expected_rendering = NULL;
+        yl_event_record_delete(&actual_events);
+        yl_event_record_delete(&expected_events);
     }
-
-    yl_event_record_delete(&actual_events);
-    yl_event_record_delete(&expected_events);
-
+done:
     return 1;
 
 error:
@@ -129,5 +121,27 @@ error:
     yl_event_record_delete(&actual_events);
     yl_event_record_delete(&expected_events);
     yaml_event_delete(&next_event);
+    return 0;
+}
+
+int yl_test_record_document(yl_execution_context_t *ctx, yaml_event_t *next_event, yl_event_record_t *event_record)
+{
+    yl_event_consumer_t saved_consumer = ctx->consumer;
+
+    ctx->consumer.callback = (yl_event_consumer_callback_t *)yl_record_event;
+    ctx->consumer.data = event_record;
+
+    if (!yl_execute_document(ctx, next_event))
+        goto error;
+
+    ctx->consumer = saved_consumer;
+    yaml_event_delete(next_event);
+
+    return 1;
+
+error:
+    ctx->consumer = saved_consumer;
+    yaml_event_delete(next_event);
+
     return 0;
 }
